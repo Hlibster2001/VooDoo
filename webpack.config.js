815f2548706fa0d49e640fa7928d669fa527e0b1
Hlib Tulupov <glebtulupov2001@gmail.com>
2023-10-05 20:15:30 +0300

    webpack fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,51 @@
-const path = require('path')
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+// const path = require('path')
+// const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+// module.exports = {
+//   mode: 'development',
+//   entry: './src/index.js',
+//   output: {
+//     path: path.resolve(__dirname, 'dist'),
+//     filename: 'index.js',
+//     clean: true
+//   },
+//   devServer: {
+//     static: {
+//       directory: path.resolve(__dirname, 'src'),
+//     },
+//     port: 3000,
+//     open: true,
+//     hot: true,
+//     compress: true,
+//     historyApiFallback: true,
+//   },
+//   plugins: [
+//     new HtmlWebpackPlugin({
+//       template: "src/index.html",
+//     }),
+//   ],
+//   module: {
+//     rules: [
+//       {
+//         test: /\.css$/i,
+//         include: path.resolve(__dirname, 'src'),
+//         use: ['style-loader', 'css-loader', 'postcss-loader'],
+//       },
+//       {
+//         test: /\.html$/,
+//         loader: "html-loader",
+//       },
+//     ],
+//   },
+// }
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'index.js',
-    clean: true
-  },
+
   devServer: {
     static: {
       directory: path.resolve(__dirname, 'src'),
@@ -21,21 +58,19 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "src/index.html",
+      template: 'src/index.html',
     }),
   ],
   module: {
     rules: [
       {
         test: /\.css$/i,
-        include: path.resolve(__dirname, 'src'),
         use: ['style-loader', 'css-loader', 'postcss-loader'],
       },
       {
         test: /\.html$/,
-        loader: "html-loader",
+        use: 'html-loader',
       },
     ],
   },
-}
-
+};
